8e3e8d0465429ecbff49e67884bae0bed8670480
Ahmmmyoo <ahmadirfankhan@gmail.com>
2022-05-16 19:10:28 +0500

    new on Random00.java

--- Random00.java
+++ Random00.java
@@ -3,18 +3,8 @@ import java.util.Scanner;
 public class Random00 {
 	public static void main(String args[]) {
 
-		Scanner s = new Scanner(System.in);
 
-		System.out.print("Enter Number : ");
-		int c = s.nextInt();
-
-		System.out.print("Enter Number : ");
-		int v = s.nextInt();
-
-		System.out.print("\nSum: " + (c + v));
-		System.out.print("\nDif: " + (c - v));
-		System.out.print("\nMul: " + (c * v));
-		System.out.print("\nDiv: " + (c / v));
+		System.out.print("Trying someting new");
 
 	}
 }
