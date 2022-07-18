b4a35e1e7e0e419f56b45cb169f01a7cef2ab4f4
Ahmmmyoo <ahmadirfankhan@gmail.com>
2022-07-18 22:46:39 +0500

    ArrayList Collection.sort forEachLoop

--- Random00.java
+++ Random00.java
@@ -1,28 +1,50 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Random00 {
 	public static void main(String args[]) {
 
 
 		System.out.print("\nTrying someting new\n");
 
-		System.out.println("Starting Values : a=3  b=5  c=1  x=2  y=-2  z=6");
-		int a, b, c, x, y, z ;
-		int ans = 0; // to store answer of each question
-		int v1, v2, v3, v4;
+		ArrayList<String> alf = new ArrayList<>();
+		
+		alf.add("A");
+		alf.add("C");
+		alf.add("B");
+		alf.add("E");
+		alf.add("D");
+		alf.add("G");
+		alf.add("F");
 
-		// Q3
-		a=3; b=5; c=1; x=2; y=-2; z=6; // initializing values
-		v1 = x;
-		v2 = ++x;
-		v3 = x--;
-		v4 = x;
-		System.out.println(" a:"+a+" b:"+b+" c:"+c+" x:"+x+" y:"+y+" z:"+z);
-		System.out.println(" v1:"+v1+" v2:"+v2+" v3:"+v3+" v4:"+v4);
-		ans = x + ++x * x-- ;
-		System.out.println("Answer 3: "+ans);
-		ans = v1 + v4 * v3;
-		System.out.println("Answer 3:(wro) "+ans);
-		ans = v1 + v2 * v3;
-		System.out.println("Answer 3:(acc) "+ans);
+		System.out.println(" *** ");
+
+		for(int i=0; i<alf.size(); i++) //for loop
+		System.out.println(alf.get(i));
+
+		System.out.println(" *** ");
+		
+		for (String str : alf) // for each loop
+		System.out.println(str);
+		
+		System.out.println(" *** ");
+		
+		Collections.sort(alf); // sorting
+		System.out.println("sorting...");
+		
+		System.out.println(" *** ");
+
+		for(int i=0; i<alf.size(); i++) //for loop
+		System.out.println(alf.get(i));
+
+		System.out.println(" *** ");
+		
+		for (String str : alf) // for each loop
+		System.out.println(str);
+		
+		System.out.println(" *** ");
+
+		
 
 	
 
